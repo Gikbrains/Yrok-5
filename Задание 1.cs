6404988125522c362bb1d9d3e21e6fcf63eda359
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-03-06 00:13:18 +0300

    Add files via upload

---
+++ Задание 1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void ConsoleApp14()
+            {
+                Cosole.WriteLine("HEllol");
+            }
+
+            static void PrintHouseNumber(Building house)
+            {
+                Console.WriteLine(house?.Address?.Split(' ')[1]);
+            }
+
+            
+        }
+    }
+}
