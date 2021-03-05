bd25d7dc9942a81b3b0e73305bc1371c3cb9d1e2
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-03-06 00:13:40 +0300

    Add files via upload

---
+++ Задание 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static vid AppendTime()
+            {
+                using var sw = File.AppendText("startup.txt");
+                var time = DataTime.Now.ToString("HH:mm:ss tt");
+                sw.writeLine(time);
+                Console.WriteLine($"I add to startup.txt new time, it's {time}, go check");
+            }
+        }
+    }
+}
